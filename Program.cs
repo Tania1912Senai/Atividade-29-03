3607e01697547c479d7cf6a4e076de17fa60249b
Tania De Oliveira Lima Dos Santos <1185849874@info.local>
2023-03-29 11:00:44 -0300

    29/03

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿//Programa para calcular a idade de uma pessoa em meses, dias, horas e minutos
+
+//Entrada de dados
+
+//Console.WriteLine ("digite nome:");
+//string nome = Console.ReadLine();
+//Console.WriteLine ("digite idade:");
+//int idade = int.Parse(Console.ReadLine());
+//int meses = idade * 12;
+//int dias = idade * 365;
+//int horas = dias * 24;
+//int minutos = horas * 60;
+//Console.WriteLine ($"sua idade corresponde a: {idade} anos, ou {meses} meses, ou {dias} dias, ou {horas} horas, ou {minutos} minutos!");
+
+//Atividade para entregar
+//Entrada de dados
+Console.WriteLine ("digite Aluno");
+string aluno = Console.ReadLine();
+
+Console.WriteLine($"digite o valor da nota 1");
+float n1 = float.Parse(Console.ReadLine());
+Console.WriteLine($"digite o valor da nota 2");
+float n2 = float.Parse(Console.ReadLine());
+Console.WriteLine($digite o valor da nota 3);
+float n3 = float.Parse()
+
+
+
+
+float media = (n1 + n2 + n3 + n4 + n5)/5
+
+
+
+
+
+
+
+
+
+
